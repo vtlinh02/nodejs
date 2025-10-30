93abd025c6b6c7c6167fb677447a24e222bb2f01
TonyVu <vutuanlinh02122002@gmail.com>
2025-10-30 16:04:00 +0700

    file server completed

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import * as http from 'node:http'
 import {stat} from 'node:fs/promises'
 import * as fs from 'node:fs'
+import path from 'node:path'
 
 const PORT = 8000
 
@@ -20,15 +21,27 @@ const requestHandle = async (req , res) => {
 
   // 2. Get static resources
   if(req.method === 'GET' && req.url.includes('/static/')){
-    const filePath = 'compute it later'
+    const fileName = req.url.substring('/static/'.length)
+    const filePath = path.join(import.meta.dirname, 'static', fileName)
 
     try {
       const info = await stat(filePath)
 
+      const fileExtension = fileName.substring(fileName.lastIndexOf('.'))
+
+      let contentType = null;
+      switch(fileExtension){
+        case '.html':
+          contentType = 'text/html; charset=utf-8'
+          break
+        default:
+          contentType = 'text/plain; charset=utf-8'
+      }
+
       res.writeHead(200, {
         'Content-Length': info.size,
-        'Content-Type': 'text/plain; charset=utf-8'
-        //...
+        'Content-Type': contentType,
+        'Content-Disposition': `inline; filename*=UTF-8''${encodeURIComponent(fileName)}`
       })
 
       const readStream = fs.createReadStream(filePath)
@@ -59,7 +72,6 @@ const requestHandle = async (req , res) => {
         res.end('Internal server error')
       }
     }
-
   }
 
 };
